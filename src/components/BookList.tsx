31e67924c56f3a845f8fee39243de8d4808d6c87
Aleksey Abamzarov <alekseydevweb@gmail.com>
2023-10-10 02:08:48 +0300

    feat: main logic (with errors)

---
+++ src/components/BookList.tsx
@@ -0,0 +1,35 @@
+'use client';
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { loadMoreBooks } from '../redux/actions';
+import Book from './Book';
+
+const BookList = () => {
+  const [startIndex, setStartIndex] = useState(30);
+  const books = useSelector((state: { books: any[] }) => state.books);
+  const loading = useSelector((state: { loading: boolean }) => state.loading);
+  const error = useSelector((state: { error: any }) => state.error);
+  const dispatch = useDispatch();
+
+  const handleLoadMore = () => {
+    dispatch(loadMoreBooks(query, startIndex));
+    setStartIndex(startIndex + 30);
+  };
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error! {error.message}</div>;
+  }
+
+  return (
+    <div>
+      {books && books.map((book) => <Book key={book.id} book={book} />)}
+      <button onClick={handleLoadMore}>Load more</button>
+    </div>
+  );
+};
+
+export default BookList;
